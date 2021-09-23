6d29852ad0ba9715227c5ff5f5a2d42faaafc09b
km4 <m1leskm4@gmail.com>
2021-09-24 00:57:15 +0200

    init new look

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <slot/>
-  </div>
+  <div><slot></slot></div>
 </template>
 
 <static-query>
@@ -13,29 +11,7 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
+#app {
+  overflow: hidden;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,271 +1,80 @@
 <template>
   <Layout>
-    <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white">
-      <div class="container">
-
-        <!-- Brand -->
-        <a class="navbar-brand" href="./index.html">
-          <g-image alt="" width="50px" src="~/logo.png" class="navbar-brand-img"/>
-        </a>
-
-        <!-- Toggler -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <!-- Collapse -->
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-
-          <!-- Toggler -->
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fe fe-x"></i>
-          </button>
-
-          <!-- Button -->
-<!--          <a class="navbar-btn btn btn-sm btn-primary lift ms-auto" href="https://themes.getbootstrap.com/product/landkit/" target="_blank">-->
-<!--            Subscribe-->
-<!--          </a>-->
-
+    <div class="container-2 w-container">
+      <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease"
+           role="banner" class="navigation w-nav">
+        <div class="navigation-items"><a href="/" class="logo-link w-nav-brand" aria-label="home"><img
+            src="https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdce5a2f79ea199f4aef1_Vector.png"
+            width="20" alt="" class="logo-image"></a><a href="#" class="link-2">hopscoders</a>
+          <div class="navigation-wrap">
+            <div class="menu-button w-nav-button" style="-webkit-user-select: text;" aria-label="menu" role="button"
+                 tabindex="0" aria-controls="w-nav-overlay-0" aria-haspopup="menu" aria-expanded="false"><img
+                src="https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdbddf55d55840a36b848_menu-icon.png"
+                width="22" alt="" class="menu-icon"></div>
+          </div>
         </div>
-
+        <div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0" style="height: 1797px; display: block;"></div>
       </div>
-    </nav>
-
-    <!-- WELCOME -->
-    <section class="pt-4 pt-md-11">
+    </div>
+    <div class="section">
       <div class="container">
-        <div class="row align-items-center">
-          <div class="col-12 col-md-5 col-lg-6 order-md-2">
+        <div class="w-layout-grid about-intro-grid">
+          <div id="w-node-_9b2922ad-09b1-aebb-0430-7dee623c62c1-5d36b83e" class="about-head-text-wrap">
+            <div class="label cc-about-light">Hey there! 🍻</div>
+            <h2 class="hi-there-heading">Coming soon !</h2>
+            <p class="paragraph-light">We are creating something special for you. Hopscoders is the first open source
+              project made for the love of beer and code.</p>
+            <p class="paragraph-light">
+              We are passioned about homebrewing and we are trying to create community around it.
+            </p></div>
+          <img src="https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdd6b676ec13b0f3118ce_Group.png"
+               id="w-node-_9b2922ad-09b1-aebb-0430-7dee623c62c8-5d36b83e"
+               sizes="(max-width: 479px) 94vw, (max-width: 767px) 96vw, (max-width: 1070px) 94vw, 1006px"
+               srcset="https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdd6b676ec13b0f3118ce_Group-p-500.png 500w, https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdd6b676ec13b0f3118ce_Group.png 1006w"
+               alt=""></div>
+      </div>
+    </div>
+    <!--    <div class="section cc-contact">-->
+    <!--      <div class="container">-->
+    <!--        <div class="contact">-->
+    <!--          <div class="contact-headline"><h3>Want to get in touch?<br>Drop me a line!</h3>-->
+    <!--            <p class="paragraph-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p></div>-->
+    <!--          <div class="contact-form-wrap">-->
+    <!--            <div class="w-form">-->
+    <!--              <form id="wf-form-Email-Form" name="wf-form-Email-Form" data-name="Email Form" class="contact-form">-->
+    <!--                <div class="w-layout-grid contact-form-grid">-->
+    <!--                  <div id="w-node-d783a17e-0b35-a13a-0448-a852d0df4a32-d0df4a24"><label for="Name-3">Name</label><input-->
+    <!--                      type="text" class="text-field w-input" maxlength="256" name="Name" data-name="Name"-->
+    <!--                      placeholder="Enter your name" id="Name"></div>-->
+    <!--                  <div id="w-node-d783a17e-0b35-a13a-0448-a852d0df4a36-d0df4a24"><label for="Email-3">Email-->
+    <!--                    Address</label><input type="email" class="text-field w-input" maxlength="256" name="Email"-->
+    <!--                                          data-name="Email" placeholder="Enter your email" id="Email" required=""></div>-->
+    <!--                  <div id="w-node-d783a17e-0b35-a13a-0448-a852d0df4a3a-d0df4a24"><label for="Message">Form Label</label><textarea-->
+    <!--                      id="Message" name="Message" placeholder="Enter your message" maxlength="5000" data-name="Message"-->
+    <!--                      class="text-field cc-textarea w-input"></textarea></div>-->
+    <!--                </div>-->
+    <!--                <input type="submit" value="Submit" data-wait="Please wait..." class="button w-button"></form>-->
+    <!--              <div class="status-message cc-success-message w-form-done">-->
+    <!--                <div>Thank you! Your submission has been received!</div>-->
+    <!--              </div>-->
+    <!--              <div class="status-message cc-error-message w-form-fail">-->
+    <!--                <div>Oops! Something went wrong while submitting the form.</div>-->
+    <!--              </div>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <div class="footer-wrap">
+      <div><a href="https://webflow.com/" target="_blank" class="webflow-link w-inline-block"><img
+          src="https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/614cdce5a2f79ea199f4aef1_Vector.png" width="15"
+          alt="" class="webflow-logo-tiny">
+        <div class="paragraph-tiny">Powered by hopscoders</div>
+      </a></div>
+    </div>
 
-            <!-- Image -->
-            <g-image alt="" src="~/illustration.png" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" />
-<!--            <img src="assets/img/illustrations/illustration-2.png" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="..." data-aos="fade-up" data-aos-delay="100">-->
-
-          </div>
-          <div class="col-12 col-md-7 col-lg-6 order-md-1" data-aos="fade-up">
-
-            <!-- Heading -->
-            <h1 class="display-3 text-center text-md-start" style="color: #662924">
-              Hi there! 🍻
-            </h1>
-
-            <!-- Text -->
-            <p class="lead text-center text-md-start mb-6 mb-lg-8" style="color: #662924">
-              We are creating something special for you.
-              Hopscoders is the first open source project made
-              for the love of beer and code.
-            </p>
-            <p class="lead text-center text-md-start mb-6 mb-lg-8" style="color: #662924">
-
-              We are passioned about homebrewing and
-              we are trying to create community around it.
-            </p>
-            <p class="lead text-center text-md-start mb-6 mb-lg-8" style="color: #662924">
-              If you are inrested about our plans and actions,
-              leave us your email.
-            </p>
-
-            <!-- Buttons -->
-<!--            <div class="text-center text-md-start">-->
-
-<!--              <a href="#" class="btn btn-primary">-->
-<!--                Subscribe-->
-<!--              </a>-->
-<!--            </div>-->
-
-          </div>
-        </div> <!-- / .row -->
-      </div> <!-- / .container -->
-    </section>
-
-
-    <!-- FOOTER -->
-<!--    <footer class="py-8 py-md-11 bg-gray-200">-->
-<!--      <div class="container">-->
-<!--        <div class="row">-->
-<!--          <div class="col-12 col-md-4 col-lg-3">-->
-
-<!--            &lt;!&ndash; Brand &ndash;&gt;-->
-<!--            <g-image alt="Example image" src="~/logo.png" class="footer-brand img-fluid mb-2"/>-->
-
-<!--            &lt;!&ndash; Text &ndash;&gt;-->
-<!--            <p class="text-gray-700 mb-2">-->
-<!--              A better way to build.-->
-<!--            </p>-->
-
-<!--            &lt;!&ndash; Social &ndash;&gt;-->
-<!--            <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">-->
-<!--              <li class="list-inline-item list-social-item me-3">-->
-<!--                <a href="#!" class="text-decoration-none">-->
-<!--                  <img src="" class="list-social-icon" alt="...">-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="list-inline-item list-social-item me-3">-->
-<!--                <a href="#!" class="text-decoration-none">-->
-<!--                  <img src="" class="list-social-icon" alt="...">-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="list-inline-item list-social-item me-3">-->
-<!--                <a href="#!" class="text-decoration-none">-->
-<!--                  <img src="" class="list-social-icon" alt="...">-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="list-inline-item list-social-item">-->
-<!--                <a href="#!" class="text-decoration-none">-->
-<!--                  <img src="" class="list-social-icon" alt="...">-->
-<!--                </a>-->
-<!--              </li>-->
-<!--            </ul>-->
-
-<!--          </div>-->
-<!--          <div class="col-6 col-md-4 col-lg-2">-->
-
-<!--            &lt;!&ndash; Heading &ndash;&gt;-->
-<!--            <h6 class="fw-bold text-uppercase text-gray-700">-->
-<!--              Products-->
-<!--            </h6>-->
-
-<!--            &lt;!&ndash; List &ndash;&gt;-->
-<!--            <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Page Builder-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  UI Kit-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Styleguide-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Documentation-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li>-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Changelog-->
-<!--                </a>-->
-<!--              </li>-->
-<!--            </ul>-->
-
-<!--          </div>-->
-<!--          <div class="col-6 col-md-4 col-lg-2">-->
-
-<!--            &lt;!&ndash; Heading &ndash;&gt;-->
-<!--            <h6 class="fw-bold text-uppercase text-gray-700">-->
-<!--              Services-->
-<!--            </h6>-->
-
-<!--            &lt;!&ndash; List &ndash;&gt;-->
-<!--            <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Documentation-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Changelog-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Pagebuilder-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li>-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  UI Kit-->
-<!--                </a>-->
-<!--              </li>-->
-<!--            </ul>-->
-
-<!--          </div>-->
-<!--          <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">-->
-
-<!--            &lt;!&ndash; Heading &ndash;&gt;-->
-<!--            <h6 class="fw-bold text-uppercase text-gray-700">-->
-<!--              Connect-->
-<!--            </h6>-->
-
-<!--            &lt;!&ndash; List &ndash;&gt;-->
-<!--            <ul class="list-unstyled text-muted mb-0">-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Page Builder-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  UI Kit-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Styleguide-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Documentation-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Changelog-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Documentation-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li>-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Changelog-->
-<!--                </a>-->
-<!--              </li>-->
-<!--            </ul>-->
-
-<!--          </div>-->
-<!--          <div class="col-6 col-md-4 col-lg-2">-->
-
-<!--            &lt;!&ndash; Heading &ndash;&gt;-->
-<!--            <h6 class="fw-bold text-uppercase text-gray-700">-->
-<!--              Legal-->
-<!--            </h6>-->
-
-<!--            &lt;!&ndash; List &ndash;&gt;-->
-<!--            <ul class="list-unstyled text-muted mb-0">-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Documentation-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li class="mb-3">-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Changelog-->
-<!--                </a>-->
-<!--              </li>-->
-<!--              <li>-->
-<!--                <a href="#!" class="text-reset">-->
-<!--                  Pagebuilder-->
-<!--                </a>-->
-<!--              </li>-->
-<!--            </ul>-->
-
-<!--          </div>-->
-<!--        </div> &lt;!&ndash; / .row &ndash;&gt;-->
-<!--      </div> &lt;!&ndash; / .container &ndash;&gt;-->
-<!--    </footer>-->
-
+    <!--[if lte IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
   </Layout>
 </template>
 
@@ -278,7 +87,5 @@ export default {
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
-}
+@import url(https://uploads-ssl.webflow.com/614cdbddf55d55aaaf36b837/css/hopscoders.webflow.998202329.css)
 </style>
